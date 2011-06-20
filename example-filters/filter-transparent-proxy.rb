915f5c4e635b968d6e1ad3e0289aa678268bcab1
Frank DENIS <github@pureftpd.org>
2011-06-20 21:26:35 +0200

    Add a sample transparent proxy.

---
+++ example-filters/filter-transparent-proxy.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/env ruby
+
+# A sample filter that redirects every connection to port 80
+# to 127.0.0.1 port 8080
+
+require "msgpack"
+
+pac = MessagePack::Unpacker.new
+loop do
+  begin
+    data = STDIN.readpartial(65536)
+  rescue EOFError
+    break
+  end
+  pac.feed(data)
+  pac.each do |obj|    
+    if (obj["remote_port"] || nil) == 80
+      obj["remote_port"] = 8080
+      obj["remote_host"] = "127.0.0.1"
+    else
+      obj = { version: obj["version"] }
+    end
+    STDOUT.write obj.to_msgpack
+    STDOUT.flush
+  end
+end
