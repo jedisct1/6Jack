6d21281cb428080f30483fe10c1abb4f566a73ab
Anthony Catel <a.catel@weelya.com>
2012-05-24 11:35:31 +0200

    Fix build on OSX.

--- src/hook-writev.h
+++ src/hook-writev.h
@@ -2,6 +2,8 @@
 #ifndef __HOOK_WRITEV_H__
 #define __HOOK_WRITEV_H__ 1
 
+#include <sys/uio.h>
+
 DLL_PUBLIC ssize_t INTERPOSE(writev)(int fd, const struct iovec *iov, int iovcnt);
 extern ssize_t (* __real_writev)(int fd, const struct iovec *iov, int iovcnt);
 int __real_writev_init(void);
